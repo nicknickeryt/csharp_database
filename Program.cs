83757d81e2e7067edc56f563256c9037ebcdda11
NickNicker <nick4nicker@gmail.com>
2024-01-14 23:52:58 +0100

    Some more examples

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿//TODO add class for data cell
-
+using static BazaDanych.Utils;
 namespace BazaDanych
 {
     class Program
@@ -15,27 +15,80 @@ namespace BazaDanych
             Header name = new Header("Name", DataType.STRING, 16);
             Header surname = new Header("Surname", DataType.STRING, 16);
 
+            Header isStillWorking = new Header("isStillWorking", DataType.BOOLEAN, 1);
+
 
             Table table = new Table("pracownicy");
 
             table.addHeader(id);
             table.addHeader(name);
             table.addHeader(surname);
+            table.addHeader(isStillWorking);
 
-            table.printHeader(true);
+            //table.printHeader(true);
 
             Database database = new Database();
 
             database.AddTable(table);
 
 
-            table.AddRow(1, "Jan", "Kowalski");
+            
 
-            table.AddRow(2, "Damian", "Nowak");
+            table.AddRow(1, "Jan", "Kowalski", true);
 
-            table.printColumns();
+            table.AddRow(2, "Damian", "Nowak", false);
 
-            table.printRows();
+            table.AddRow(2, "Alan", "Nowak", true);
+
+            //table.printColumns();
+
+            //table.printRows();
+
+            table.printTable();
+
+
+
+
+            Header id1 = new Header("ID", DataType.INT, 10);
+            Header nazwa = new Header("Model", DataType.STRING, 16);
+            Header year = new Header("Rok produkcji", DataType.INT, 4);
+
+            Table table1 = new Table("samoloty");
+
+            table1.addHeader(id1);
+            table1.addHeader(nazwa);
+            table1.addHeader(year);
+
+            database.AddTable(table1);
+
+            table1.AddRow(1, "Antonov 223", 2004);
+
+            table1.AddRow(2, "Ił-15", 1983);
+
+            table1.AddRow(8, "PZL-Świdnik M-3", 2013);
+
+            table1.AddRow(9, "Boeing 737", 1999);
+            table1.AddRow(10, "PZL-23 Sokół", 2004);
+
+
+            table1.printTable();
+
+            table1.PrintRowByHeaderValue(year, 2004);
+
+            Dictionary<Header, List<object>> results = table1.GetRowByHeaderValue(year, 2004);
+
+            Dictionary<Header, List<object>> resultsEmployees = table.GetRowByHeaderValue(isStillWorking, true);
+
+            table.PrintRowByHeaderValue(isStillWorking, true);
+            
+            foreach(object o in resultsEmployees[name]){
+                printLine(o.ToString());
+            }
+
+            foreach(object o in results[nazwa]){
+                printLine(o.ToString());
+            }
+            
 
         }
     }
